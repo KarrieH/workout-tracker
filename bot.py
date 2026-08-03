9d89bc7bc85fe7b2c8e3d2dfb9730a73c10b6838
KarrieH <hanovakm@gmail.com>
2026-08-03 22:12:34 +0300

    fix: handle API errors and state loss in bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,8 @@ from aiogram.types import Message
 load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 API_URL = os.getenv('API_URL')
-
+API_ERROR_MSG = "Сервис сейчас временно недоступен. Попробуйте позже."
+ERROR_MSG_LOST_STATE = "начните заново с команды /workout"
 dp = Dispatcher()
 
 class WorkoutForm(StatesGroup): # Before handle any states you will need to specify what kind of states you want to handle
@@ -24,10 +25,16 @@ class WorkoutForm(StatesGroup): # Before handle any states you will need to spec
     waiting_for_workout_type = State()
     waiting_for_duplicate_confirm = State()
 
-async def get_workout_types(API_URL):         # делаем запрос к API чтобы получить виды тренировок
-    async with aiohttp.ClientSession() as session:
-        async with session.get(f"{API_URL}/api/workout-types") as response:
-            data = await response.json()
+async def get_workout_types(API_URL):   # делаем запрос к API чтобы получить виды тренировок
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f"{API_URL}/api/workout-types") as response:
+                response.raise_for_status()  # ошибки 4хх и 5хх
+                data = await response.json()
+    except aiohttp.ClientError as e:
+            print(f"Ошибка обращения к API: {e}")
+            data = None
+
     return data
 
 
@@ -52,9 +59,7 @@ async def cmd_workout(message: Message,               # когда придёт 
 
     except aiohttp.ClientError as e:
         print(f"Ошибка обращения к API: {e}")
-        await message.answer(
-            "Сервис сейчас временно недоступен. Попробуйте позже."
-        )
+        await message.answer(API_ERROR_MSG)
         return
 
     await state.update_data(user_name=data['name'],
@@ -79,28 +84,29 @@ async def cmd_workout(message: Message,               # когда придёт 
 async def cmd_workout_date_today(message: Message, state: FSMContext):
     data = await state.get_data()
 
-    user_name = data['user_name']
-    workout_date = date.today()
+    user_name = data.get('user_name')
+    if user_name is None:
+        await message.answer(ERROR_MSG_LOST_STATE)
+        return
 
+    workout_date = date.today()
     list_of_workout_types = await get_workout_types(API_URL)
+    there_is_an_error = list_of_workout_types is None # если None, то значит есть ошибка при вызове get_workout_types
+
+    if there_is_an_error:
+        await message.answer(API_ERROR_MSG)
+        return
 
     await state.update_data(workout_date=workout_date,
-                                workout_types=list_of_workout_types)
-    await state.set_state(
-            WorkoutForm.waiting_for_workout_type)  # устанавливаем состояние ожидания данных о типе тренировки
-
-
-
-
-
-
+                            workout_types=list_of_workout_types)
+    await state.set_state(WorkoutForm.waiting_for_workout_type)  # устанавливаем состояние ожидания данных о типе тренировки
 
     await message.answer(f"{user_name}, какая сегодня была тренировка?",
                          reply_markup=ReplyKeyboardMarkup(
                              keyboard=[[KeyboardButton(text=item['name'])] for item in list_of_workout_types],
                              resize_keyboard=True,
-                         )
-                         )
+                             )
+                        )
 
 
 
@@ -108,8 +114,10 @@ async def cmd_workout_date_today(message: Message, state: FSMContext):
             F.text == "Другой день")
 async def cmd_workout_date_anoter_day(message: Message, state: FSMContext):
     data = await state.get_data()
-    user_name = data['user_name']
-
+    user_name = data.get('user_name')
+    if user_name is None:
+        await message.answer(ERROR_MSG_LOST_STATE)
+        return
     await state.set_state(WorkoutForm.waiting_for_another_date)  # устанавливаем состояние ожидания данных о дате, если дата не сегодня
     await message.answer(f"Введите дату в формате ДД.ММ.ГГГГ", reply_markup=ReplyKeyboardRemove()) # тут нужно как-то записать пользовательский ввод в переменную workout_anoter_day_date
 
@@ -124,7 +132,10 @@ async def cmd_unknown_write(message: Message):
 @dp.message(WorkoutForm.waiting_for_another_date)
 async def cmd_anoter_date(message: Message, state: FSMContext) -> None:
     data = await state.get_data()
-    user_name = data['user_name']
+    user_name = data.get('user_name')
+    if user_name is None:
+        await message.answer(ERROR_MSG_LOST_STATE)
+        return
 
     user_answer = message.text
     try:
@@ -137,58 +148,95 @@ async def cmd_anoter_date(message: Message, state: FSMContext) -> None:
         )
         return
     list_of_workout_types = await get_workout_types(API_URL)
+    there_is_an_error = list_of_workout_types is None # если None, то значит есть ошибка при вызове get_workout_types
+
+    if there_is_an_error:
+        await message.answer(API_ERROR_MSG)
+        return
+
     await state.update_data(workout_date=workout_anoter_day_date,
                             workout_types=list_of_workout_types)
-
     await state.set_state(WorkoutForm.waiting_for_workout_type) # устанавливаем состояние ожидания данных о типе тренировки
 
     await message.answer(f"{user_name}, какая сегодня была тренировка?",
                          reply_markup=ReplyKeyboardMarkup(
                              keyboard=[[KeyboardButton(text=item['name'])] for item in list_of_workout_types],
                              resize_keyboard=True,
-                         )
-                         )
+                            )
+                        )
 
 @dp.message(WorkoutForm.waiting_for_workout_type)
 async def cmd_workout_type(message: Message,
                            state: FSMContext):
-    user_answer_workout_type = message.text.casefold()
-    await state.update_data(workout_type=user_answer_workout_type)
-
     data = await state.get_data()
-    user_id = data['user_id']
+    user_id = data.get('user_id')
+    workout_types = data.get('workout_types')
+    if user_id is None or workout_types is None: # проверка на слет state
+        await message.answer(ERROR_MSG_LOST_STATE)
+        return
+    user_answer_workout_type = message.text.casefold()
 
-    async with (aiohttp.ClientSession() as session):
-        async with session.get(
-                f"{API_URL}/api/check_workout_date",
-                params= {"user_id": user_id, "workout_date":data['workout_date'].isoformat()}) as response:
-            result = await response.json()
-    if len(result) == 0:  # если еще не было записаей о тренировках на указанную дату
+    str_format_workout_types = [item['name'] for item in workout_types]
 
-        body_data = {
-            "user_id": int(data['user_id']),
-            "workout_date": data['workout_date'].isoformat(),
-            "workout_type_id": [item['id'] for item in data['workout_types'] if item['name'] == user_answer_workout_type][0]
-        }
-        async with aiohttp.ClientSession() as session:
-            async with session.post(f"{API_URL}/api/workouts", json = body_data) as response: # отправляем POST запрос
+    if user_answer_workout_type not in str_format_workout_types: # проверка пользовательского ввода (опечатался, или прислал что-то свое, а не нажал кнопку с клавиатуры)
+        await state.set_state(WorkoutForm.waiting_for_workout_type)  # устанавливаем состояние ожидания повторной отправки типа тренировки
+        print(f"Ошибка ввода типа тренировки, пользователь ввел {user_answer_workout_type}")
+        await message.answer(
+            "Вы ввели неверный тип тренировки.\n"
+            "Выберите тип тренировки из предложенного")  # тут нужно вызывать  await message.answer(f"{user_name}, какая сегодня была тренировка?"
+        return
+
+    await state.update_data(workout_type=user_answer_workout_type)
+    try:
+        async with (aiohttp.ClientSession() as session):
+            async with session.get(
+                    f"{API_URL}/api/check_workout_date",
+                    params= {"user_id": user_id, "workout_date":data.get('workout_date').isoformat()}) as response:
+                response.raise_for_status()  # если 500, 403 и т.п.
                 result = await response.json()
-                print(response.status)
-                print(result)
 
-        await message.answer("Тренировка записана!", reply_markup=ReplyKeyboardRemove())
-        await state.clear()
+    except aiohttp.ClientError as e:
+        print(f"Ошибка обращения к API: {e}")
+        await message.answer(API_ERROR_MSG)
+        return
+
+
+    if len(result) == 0:  # если еще не было записаей о тренировках на указанную дату
+        try:
+            body_data = {
+                "user_id": int(data.get('user_id')),
+                "workout_date": data.get('workout_date').isoformat(),
+                "workout_type_id": [item['id'] for item in data.get('workout_types') if item['name'] == user_answer_workout_type][0]
+                }
+
+            async with aiohttp.ClientSession() as session:
+                async with session.post(f"{API_URL}/api/workouts", json = body_data) as response: # отправляем POST запрос
+                    response.raise_for_status()
+                    print(response.status)
+                    print(await response.json())
+                    await message.answer("Тренировка записана!", reply_markup=ReplyKeyboardRemove())
+                    await state.clear()
+
+        except aiohttp.ClientError as e:
+            print(f"Ошибка обращения к API: {e}")
+            await message.answer(API_ERROR_MSG)
+            await state.clear()
+            return
+
     else:
         await state.set_state(WorkoutForm.waiting_for_duplicate_confirm)  # устанавливаем состояние ожидания данных повторной отправке даты
-        await message.answer(f"На эту дату {data['workout_date']} уже есть тренировка. Вы можете добавить не более 1й тренировки в день. Хочешь записать на другой день?" ,
-                             reply_markup=ReplyKeyboardMarkup(
-                                 keyboard=[
-                                     [KeyboardButton(text="Да"),
-                                     KeyboardButton(text="Нет")]
-                                 ],
-                                 resize_keyboard=True,
-                             )
-                             )
+        await message.answer(f"На эту дату {data.get('workout_date')} уже есть тренировка. Вы можете добавить не более 1й тренировки в день. Хочешь записать на другой день?" ,
+                                     reply_markup=ReplyKeyboardMarkup(
+                                         keyboard=[
+                                             [KeyboardButton(text="Да"),
+                                             KeyboardButton(text="Нет")]
+                                         ],
+                                         resize_keyboard=True,
+                                     )
+                                     )
+
+
+
 @dp.message(WorkoutForm.waiting_for_duplicate_confirm, F.text == "Да")
 async def cmd_positive_duplicate_confirm(message: Message,
                            state: FSMContext):
